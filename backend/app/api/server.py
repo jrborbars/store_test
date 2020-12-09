7db2b1f23c3e430e8be49cbaf1e449c17749a893
JRB <jrborba.rs@gmx.com>
2020-12-09 10:00:05 -0500

    database up and runnig

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router as api_router
 
+from app.core import config, tasks
+
 def init_application():
-    app = FastAPI(title="Store_app", version="0.1.0")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -11,6 +13,10 @@ def init_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
+
     app.include_router(api_router, prefix="/api")
     return app
 
